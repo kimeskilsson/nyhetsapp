fd7d500f85eea242e83aaa3a8fb322b9854c8720
Kim Eskilsson <kim.eskilsson@gmail.com>
2019-04-01 11:04:58 +0200

    added components

---
+++ src/components/Nyhetsartikel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nyhetslista from './Nyhetslista';
+
+function Nyhetsartikel() {
+  return(<article>
+    <img src="https://source.unsplash.com/random/400x250/?dog"></img>
+    <h2> Testnyhet</h2>
+    <p> beskrivning av testnyheten</p>
+    <a> läs mer...</a>
+    </article>);
+}
+
+export default Nyhetsartikel;
